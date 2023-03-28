0232a61c6a2cc00ac277ab156bae4c9faf7d7f6c
JamesBremner <james@ravenspoint.com>
2023-03-28 10:28:49 -0400

    readSMILES  converted to pathfinder vGraph

--- src/chemgraph.cpp
+++ src/chemgraph.cpp
@@ -24,7 +24,9 @@ void cChemGraph::readSMILES(const std::string &sin)
         case 'C':
         case 'N':
         case 'O':
-            wVertexAttr(find((std::to_string(idx))),{std::to_string(token)});
+            wVertexAttr(
+                add(std::to_string(idx)),
+                {std::string(1,token)});
             idx++;
             if (src == -1)
             {
@@ -115,122 +117,122 @@ void cChemGraph::read(const std::string &sin)
         lid = 'e';
     }
 }
-std::string cChemGraph::viz()
-{
-    std::map<std::string,std::string> mpColor;
-    mpColor.insert( {"C","grey"} );
-    mpColor.insert( {"O","red"} );
-    mpColor.insert( {"N","green"} );
+// std::string cChemGraph::viz()
+// {
+//     std::map<std::string,std::string> mpColor;
+//     mpColor.insert( {"C","grey"} );
+//     mpColor.insert( {"O","red"} );
+//     mpColor.insert( {"N","green"} );
 
-    std::stringstream sviz;
-    sviz << "graph G {\n";
-    for (auto n : nodes())
-    {   
-        sviz << n.second.myName
-             << " [label=\"" << n.second.myColor
-             << "\" color = \"" << mpColor[ n.second.myColor ]
-             << "\"  penwidth = 3.0 ];\n";
-    }
-    for (auto &e : links())
-    {
-        if (e.first.first > e.first.second)
-            continue;
-        std::string color = "";
-        if( e.second.myCost == 2 )
-            color = " [penwidth = 3.0]";
-        sviz << src(e).myName << "--"
-             << dst(e).myName
-             << color
-             << "\n";
-    }
-    sviz << "}\n";
+//     std::stringstream sviz;
+//     sviz << "graph G {\n";
+//     for (auto n : nodes())
+//     {   
+//         sviz << n.second.myName
+//              << " [label=\"" << n.second.myColor
+//              << "\" color = \"" << mpColor[ n.second.myColor ]
+//              << "\"  penwidth = 3.0 ];\n";
+//     }
+//     for (auto &e : links())
+//     {
+//         if (e.first.first > e.first.second)
+//             continue;
+//         std::string color = "";
+//         if( e.second.myCost == 2 )
+//             color = " [penwidth = 3.0]";
+//         sviz << src(e).myName << "--"
+//              << dst(e).myName
+//              << color
+//              << "\n";
+//     }
+//     sviz << "}\n";
 
-    auto path = std::filesystem::temp_directory_path();
-    std::cout << "RunDOT " << path << "\n";
-    auto gdot = path / "g.dot";
-    std::ofstream f(gdot);
-    if (!f.is_open())
-        throw std::runtime_error("Cannot open " + gdot.string());
-    f << sviz.str();
+//     auto path = std::filesystem::temp_directory_path();
+//     std::cout << "RunDOT " << path << "\n";
+//     auto gdot = path / "g.dot";
+//     std::ofstream f(gdot);
+//     if (!f.is_open())
+//         throw std::runtime_error("Cannot open " + gdot.string());
+//     f << sviz.str();
 
-    f.close();
+//     f.close();
 
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
+//     STARTUPINFO si;
+//     PROCESS_INFORMATION pi;
 
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
+//     ZeroMemory(&si, sizeof(si));
+//     si.cb = sizeof(si);
+//     ZeroMemory(&pi, sizeof(pi));
 
-    auto sample = path / "sample.png";
-    std::string scmd = "dot -Kfdp -n -Tpng -Tdot -o " + sample.string() + " " + gdot.string();
+//     auto sample = path / "sample.png";
+//     std::string scmd = "dot -Kfdp -n -Tpng -Tdot -o " + sample.string() + " " + gdot.string();
 
-    // std::cout << scmd << "\n";
+//     // std::cout << scmd << "\n";
 
-    // Retain keyboard focus, minimize module2 window
-    si.wShowWindow = SW_SHOWNOACTIVATE | SW_MINIMIZE;
-    si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USEPOSITION;
-    si.dwX = 600;
-    si.dwY = 200;
+//     // Retain keyboard focus, minimize module2 window
+//     si.wShowWindow = SW_SHOWNOACTIVATE | SW_MINIMIZE;
+//     si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USEPOSITION;
+//     si.dwX = 600;
+//     si.dwY = 200;
 
-    if (!CreateProcess(NULL,                 // No module name (use command line)
-                       (char *)scmd.c_str(), // Command line
-                       NULL,                 // Process handle not inheritable
-                       NULL,                 // Thread handle not inheritable
-                       FALSE,                // Set handle inheritance to FALSE
-                       CREATE_NEW_CONSOLE,   //  creation flags
-                       NULL,                 // Use parent's environment block
-                       NULL,                 // Use parent's starting directory
-                       &si,                  // Pointer to STARTUPINFO structure
-                       &pi)                  // Pointer to PROCESS_INFORMATION structure
-    )
-    {
-        int syserrno = GetLastError();
-        if (syserrno == 2)
-        {
-            std::cout <<
-                "Cannot find executable file\n"
-                "Is graphViz installed?\n";
+//     if (!CreateProcess(NULL,                 // No module name (use command line)
+//                        (char *)scmd.c_str(), // Command line
+//                        NULL,                 // Process handle not inheritable
+//                        NULL,                 // Thread handle not inheritable
+//                        FALSE,                // Set handle inheritance to FALSE
+//                        CREATE_NEW_CONSOLE,   //  creation flags
+//                        NULL,                 // Use parent's environment block
+//                        NULL,                 // Use parent's starting directory
+//                        &si,                  // Pointer to STARTUPINFO structure
+//                        &pi)                  // Pointer to PROCESS_INFORMATION structure
+//     )
+//     {
+//         int syserrno = GetLastError();
+//         if (syserrno == 2)
+//         {
+//             std::cout <<
+//                 "Cannot find executable file\n"
+//                 "Is graphViz installed?\n";
 
-            return "";
-        }
-        std::cout << "system error\n";
-        // SetStatusText(wxString::Format("Sysem error no (%d)\n", GetLastError()));
-        // wchar_t *lpMsgBuf;
-        // FormatMessage(
-        //     FORMAT_MESSAGE_ALLOCATE_BUFFER |
-        //         FORMAT_MESSAGE_FROM_SYSTEM |
-        //         FORMAT_MESSAGE_IGNORE_INSERTS,
-        //     NULL,
-        //     (DWORD)syserrno,
-        //     MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        //     (LPWSTR)&lpMsgBuf,
-        //     0, NULL);
+//             return "";
+//         }
+//         std::cout << "system error\n";
+//         // SetStatusText(wxString::Format("Sysem error no (%d)\n", GetLastError()));
+//         // wchar_t *lpMsgBuf;
+//         // FormatMessage(
+//         //     FORMAT_MESSAGE_ALLOCATE_BUFFER |
+//         //         FORMAT_MESSAGE_FROM_SYSTEM |
+//         //         FORMAT_MESSAGE_IGNORE_INSERTS,
+//         //     NULL,
+//         //     (DWORD)syserrno,
+//         //     MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+//         //     (LPWSTR)&lpMsgBuf,
+//         //     0, NULL);
 
-        // char errorbuf[200];
-        // snprintf(errorbuf, 199,
-        //          "Error is %S",
-        //          lpMsgBuf);
-        // LocalFree(lpMsgBuf);
+//         // char errorbuf[200];
+//         // snprintf(errorbuf, 199,
+//         //          "Error is %S",
+//         //          lpMsgBuf);
+//         // LocalFree(lpMsgBuf);
 
-        return "";
-    }
+//         return "";
+//     }
 
-    // Close process and thread handles.
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
+//     // Close process and thread handles.
+//     CloseHandle(pi.hProcess);
+//     CloseHandle(pi.hThread);
 
-    Sleep(1000);
+//     Sleep(1000);
 
-    return sviz.str();
-}
+//     return sviz.str();
+// }
 
 std::map<std::string,int> cChemGraph::countAtoms() 
 {
     std::map<std::string,int> mpCount;
-    for (const auto& n : nodes())
+    for (int vi = 0; vi < vertexCount(); vi++ )
     {
-        mpCount[ n.second.myColor ]++;
+        mpCount[ rVertexAttr(vi,0) ]++;
     }
     return mpCount;
 }
@@ -238,20 +240,20 @@ std::map<std::string,int> cChemGraph::countAtoms()
     void cChemGraph::readNodeFeatures(const std::string &sin)
     {
         // each line is one feature value for every node
-        std::string line;
-        std::istringstream ssin( sin );
-        while( getline(ssin, line))
-        {
-            std::vector<int> vf;
-            std::istringstream sline( line );
-            for( int kn = 0; kn < nodeCount(); kn++ ) {
-                int v;
-                sline >> v;
-                vf.push_back( v );
+        // std::string line;
+        // std::istringstream ssin( sin );
+        // while( getline(ssin, line))
+        // {
+        //     std::vector<int> vf;
+        //     std::istringstream sline( line );
+        //     for( int kn = 0; kn < nodeCount(); kn++ ) {
+        //         int v;
+        //         sline >> v;
+        //         vf.push_back( v );
 
-            }
-            myNodeFeatures.push_back( vf );
-        }
+        //     }
+        //     myNodeFeatures.push_back( vf );
+        // }
     }
     void cChemGraph::readBondFeatures(const std::string &sin)
     {
--- src/chemgraph.h
+++ src/chemgraph.h
@@ -3,14 +3,23 @@ class cChemGraph : public raven::graph::cGraph
 {
 public:
     void read(const std::string &sin);
+
+    /// @brief read SMILES string
+    /// @param sin SMILES string
     void readSMILES(const std::string &sin);
+    
     void readNodeFeatures(const std::string &sin);
     void readBondFeatures(const std::string &sin);
     // bool fragment(
     //     const std::string &entire,
     //     const std::string &fragment);
     std::string viz();
+
+    /// @brief Count atoms of each type
+    /// @return map of atom counts keyed by atom symbol, e.g. map["C"] = number of carbon atoms
+
     std::map<std::string,int> countAtoms();
+
 private:
     std::vector< std::vector<int>> myNodeFeatures;
     std::vector< std::vector<int>> myEdgeFeatures;
